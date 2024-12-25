def14fe3853f5385cb48f91242b8bc6b0896404d
Ari <49175620+AriAlavi@users.noreply.github.com>
2024-12-25 12:36:52 -0500

    give the bot a soul

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from src.chat import AnthropicChat
 from src.discord_bot import DiscordService
 from src.integrated_bot import BotService
+from src.personality import Personality
 
 
 def main():
-    anthropic_client = AnthropicChat()
+    personality = Personality("Carl Jung", None)
+    anthropic_client = AnthropicChat(personality)
     discord_service = DiscordService()
+
     bot_service = BotService(discord_service, anthropic_client)
     bot_service.run()
 
--- src/chat.py
+++ src/chat.py
@@ -2,13 +2,15 @@ from anthropic import Anthropic
 from src.private_data import anthropic_api_key
 from src.messenger import DiscordMessage
 from typing import List
+from src.personality import Personality
 
 
 class AnthropicChat:
-    def __init__(self):
+    def __init__(self, personality: Personality):
         self.anthropic_client = Anthropic(
             api_key=anthropic_api_key(),
         )
+        self.personality = personality
 
     def send_message(self, message: str) -> str:
         response = self.anthropic_client.messages.create(
@@ -29,6 +31,12 @@ class AnthropicChat:
 
     def _message_is_whitespace(self, message: DiscordMessage) -> bool:
         return message.content.strip() == ""
+
+    def _build_system_message(self) -> str:
+        return {
+            "role": "system",
+            "content": self.personality.build_context(),
+        }
 
     def build_chat_context_two_way(self, messages: List[DiscordMessage]) -> str:
         """
@@ -56,6 +64,7 @@ class AnthropicChat:
             model="claude-3-5-sonnet-latest",
             messages=anthropic_messages,
             max_tokens=1024,
+            system=self.personality.build_context(),
         )
         print("Anthropic response:", response)
 
