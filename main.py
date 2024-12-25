b8d5315975d88881c41a4ca4f8cc4af5f47f17bd
Ari <49175620+AriAlavi@users.noreply.github.com>
2024-12-25 13:15:12 -0500

    added zizek

--- main.py
+++ main.py
@@ -9,21 +9,29 @@ def main():
     # Create personalities
     carl_jung = Personality("Carl Jung", None)
     soren_kierkegaard = Personality("Soren Kierkegaard", None)
+    slavoj_zizek = Personality(
+        "Slavoj Zizek", "Don't forget to sniff!", larping_allowed=True
+    )
 
     # Create bots
     carl_jung_bot = bot_factory.create_bot(carl_jung)
     soren_kierkegaard_bot = bot_factory.create_bot(soren_kierkegaard)
+    slavoj_zizek_bot = bot_factory.create_bot(slavoj_zizek)
 
     # Run bots
     # Start bots in separate threads
 
     jung_thread = threading.Thread(target=carl_jung_bot.run)
     kierkegaard_thread = threading.Thread(target=soren_kierkegaard_bot.run)
+    zizek_thread = threading.Thread(target=slavoj_zizek_bot.run)
+
     jung_thread.daemon = True
     kierkegaard_thread.daemon = True
+    zizek_thread.daemon = True
 
     jung_thread.start()
     kierkegaard_thread.start()
+    zizek_thread.start()
 
     # Keep main thread alive but allow keyboard interrupts
     try:
